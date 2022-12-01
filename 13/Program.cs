bf4d9a4010558a6be1c3a81be70194f661b39f3a
Natasha Khalkina <tashik2010@mail.ru>
2022-12-01 16:53:59 +0300

    Решение задачи 13

---
+++ 13/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите трехзначное число.\nВыхода из программы команда Exit.");
+ while(true)
+    {
+        string input = Console.ReadLine();
+        if(!number.Equals("exit"))
+{
+Console.WriteLine("{0}->{1}",input, input[1]);
+}
+            
+        else
+            break;
+    }
